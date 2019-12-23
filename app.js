53bff5e1d2c5203b73699705d622f422b5acb389
Shaz <shaz.ali1997@gmail.com>
2019-12-23 15:20:27 -0600

    adds mongoose client and POST/GET methods to DB

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb+srv://shaz:shaz@cluster0-spesv.mongodb.net/yelp_camp?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
+
+// Schema Setup
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-const campgrounds = [
-    { name: 'Salmon Creek', image: 'https://live.staticflickr.com/5489/10633082724_0131de79b4_m.jpg'},
-    { name: 'Ahad Creek', image: 'https://live.staticflickr.com/3698/10633084834_a625340ccc_m.jpg'},
-    { name: 'Shaz Creek', image: 'https://farm1.staticflickr.com/121/290597016_bbb430eb1a_m.jpg'},
-    { name: 'Baltimore Creek', image: 'https://live.staticflickr.com/3795/10633096196_bd02cceef9_m.jpg'},
-    { name: 'Salmon Creek', image: 'https://live.staticflickr.com/5489/10633082724_0131de79b4_m.jpg'},
-    { name: 'Ahad Creek', image: 'https://live.staticflickr.com/3698/10633084834_a625340ccc_m.jpg'},
-    { name: 'Shaz Creek', image: 'https://farm1.staticflickr.com/121/290597016_bbb430eb1a_m.jpg'},
-    { name: 'Baltimore Creek', image: 'https://live.staticflickr.com/3795/10633096196_bd02cceef9_m.jpg'},
-]
 // GET
 app.get('/', (req, res) => {
     res.render('landing');
 })
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', {campgrounds: campgrounds}); // {name: data}
+    // get all campgrounds from DB
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds', {campgrounds: allCampgrounds});
+        }
+    })
+    // res.render('campgrounds', {campgrounds: campgrounds}); // {name: data}
 })
 
 app.get('/campgrounds/new', (req, res) => {
@@ -31,13 +40,20 @@ app.get('/campgrounds/new', (req, res) => {
 
 // POST
 app.post('/campgrounds', (req, res) => {
+    console.log('req: ', req.body);
     // get data from form and add to cg array
     const name = req.body.name;
     const image = req.body.image;
     const newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
+    // create a new campground and save to DB
+    Campground.create(newCampground, (err, newlyCreated) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds');
+        }
+    })
     // redirect back to cg page
-    res.redirect('/campgrounds');
 })
 
 app.listen(port, () => {
