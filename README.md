b5587cdcce5ecd1bd31dede2ca5a2c7ba7be22f1
Shaz <shaz.ali1997@gmail.com>
2019-12-24 01:26:40 -0600

    house keeping after models and seeds

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 This is a README for Yelp Camp!
+
+RESTful ROUTES:
+name         url               verb      desc
+====================================================================
+INDEX    /campgrounds          GET       Show all Campgrounds from DB
+NEW      /campgrounds/new      GET       Show form to create new Campground
+CREATE   /campgrounds          POST      Add new campground to the DB
+SHOW     /campgrounds/:id      GET       Shows one specific campground
+
+Mongo:
+.find({}, (err, allFound) => {}) -- find all from DB
+.create(objType, (err, cb) => {}) -- create one to DB
+.findById(req.params.x, (err, cb) => {}) -- find one from DB
--- app.js
+++ app.js
@@ -3,21 +3,33 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const Campground = require('./models/campground');
+const Comment = require('./models/comment');
+const seedDB = require('./seed');
 
 mongoose.connect('mongodb+srv://shaz:shaz@cluster0-spesv.mongodb.net/yelp_camp?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
 
-// Schema Setup
-const campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String
-});
-
-const Campground = mongoose.model('Campground', campgroundSchema);
+// Campground.create(
+//     {
+//         name: "Shaz Creek",
+//         image: "https://images.unsplash.com/photo-1507720708252-1ddeb1dbff34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+//         description: "Huge hill"
+//     },
+//     (err, campground) => {
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log('created!: ' + campground);
+//         }
+//     }
+// )
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-// GET
+seedDB();
+
+// INDEX - show all campgrounds
 app.get('/', (req, res) => {
     res.render('landing');
 })
@@ -28,23 +40,24 @@ app.get('/campgrounds', (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.render('campgrounds', {campgrounds: allCampgrounds});
+            res.render('index', {campgrounds: allCampgrounds});
         }
     })
     // res.render('campgrounds', {campgrounds: campgrounds}); // {name: data}
 })
 
+// NEW - show form to create campground
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
 })
 
-// POST
+// CREATE - add new campgrounds to DB
 app.post('/campgrounds', (req, res) => {
-    console.log('req: ', req.body);
     // get data from form and add to cg array
     const name = req.body.name;
     const image = req.body.image;
-    const newCampground = {name: name, image: image};
+    const desc = req.body.description;
+    const newCampground = {name: name, image: image, description: desc}; // {name: data}
     // create a new campground and save to DB
     Campground.create(newCampground, (err, newlyCreated) => {
         if (err) {
@@ -54,6 +67,18 @@ app.post('/campgrounds', (req, res) => {
         }
     })
     // redirect back to cg page
+})
+
+// SHOW
+app.get('/campgrounds/:id', (req, res) => {
+    Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // render show template with campground
+            res.render('show', {campground: foundCampground});
+        }
+    })
 })
 
 app.listen(port, () => {
